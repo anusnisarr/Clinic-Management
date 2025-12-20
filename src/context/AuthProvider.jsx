59e7848e63d48eb4d052a8fca75bb32a23034d3c
Anas Nisar <anusnisarr@gmail.com>
2025-12-20 23:42:47 +0500

    remove auth  provider from login ans sign up

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,7 +1,8 @@
 import { createContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useLocation } from "react-router-dom";
 
+const PUBLIC_ROUTES = ["/login", "/signup"];
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
@@ -9,6 +10,7 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const location = useLocation();
   const BASE_URL = import.meta.env.VITE_BASE_PATH;
 
   const refreshAccessToken = useCallback(async () => {
@@ -22,17 +24,24 @@ export function AuthProvider({ children }) {
       setAccessToken(res.data.accessToken);
       setUser(res.data.user);
     } catch (err) {    
-      navigate("/login", { replace: true });
       setAccessToken(null);
       setUser(null);
+      if (!PUBLIC_ROUTES.includes(Location.pathname)) {
+        navigate("/login", { replace: true });
+      }
     } finally {
       setLoading(false);
     }
-  }, [BASE_URL, navigate]);
+  }, [BASE_URL, navigate, location.pathname]);
   
   useEffect(() => {
+    if (PUBLIC_ROUTES.includes(location.pathname)) {
+      setLoading(false);
+      return;
+    }
+
     refreshAccessToken();
-  }, [refreshAccessToken]);
+  }, [location.pathname , refreshAccessToken]);
 
   if (loading) return null; // or a loader component
 
